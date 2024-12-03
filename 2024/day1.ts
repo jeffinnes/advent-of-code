39ff1dda567c1bbf00356642efabf31cde8eafe4
Jeff Innes <contact.me@jeffinnes.dev>
2024-12-02 19:31:32 -0700

    Add part two solution And also a basic attempt at performance benchmarking.

--- 2024/day1.ts
+++ 2024/day1.ts
@@ -7,9 +7,13 @@ const leftList: Array<number> = [];
 const rightList: Array<number> = [];
 let runningTotalDistance: number = 0;
 
+// Part 1
+
 // Split the input string into an array of strings
 // Loop over this array to split the values and push them into the correct left or right array list
 const sideBySideListArray: Array<string> = sideBySideLists.split('\n');
+
+const partOneStart: number = Date.now();
 
 sideBySideListArray.forEach((inputRowString: String) => {
   const inputRow: Array<string> = inputRowString.split('   ');
@@ -28,4 +32,38 @@ for (let i = 0; i < sideBySideListArray.length; i++) {
   runningTotalDistance = runningTotalDistance + Math.abs(leftList[i] - rightList[i]);
 }
 
-console.log('\n\nTotal Distance Difference: ', runningTotalDistance);
+const partOneEnd: number = Date.now();
+const partOneDuration: number = partOneEnd - partOneStart;
+
+console.log('Part 1 Execution Time: ', partOneDuration, 'ms');
+console.log('\nTotal Distance Difference: ', runningTotalDistance);
+
+// Part 2
+const partTwoStart: number = Date.now();
+
+// Initialise the similarity score
+let similarityScore: number = 0;
+
+// Loop over the left list
+// For each value in the left list, find the first occurrence in the right list
+// find the number of times this value occurs in the right list by looping untill the value in the right list no longer matches
+// Multiply the value in the left list by the number of times it occured in the right list and add it to the similarity score
+leftList.forEach((leftValue: number) => {
+  let multiplier: number = 0;
+  let rightIndex: number = rightList.indexOf(leftValue);
+
+  if (rightIndex !== -1) {
+    while (leftValue === rightList[rightIndex]) {
+      multiplier += 1;
+      rightIndex += 1;
+    }
+  }
+
+  similarityScore += leftValue * multiplier;
+});
+
+const partTwoEnd: number = Date.now();
+const partTwoDuration: number = partTwoEnd - partTwoStart;
+
+console.log('\n\nPart 2 Execution Time: ', partTwoDuration, 'ms');
+console.log('\nSimilarity Score: ', similarityScore);
